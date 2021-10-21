a0aa98f5d9661d08af60a0e7162774e0fb471ec4
Maxim Talanov <mail@maxtalanov.ru>
2021-10-22 02:32:45 +0300

    feat (v.2): Инициализировать БД
    В файле app.js нужно будет инициализировать бд, именованную как "moviesdb", а так же опции.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
