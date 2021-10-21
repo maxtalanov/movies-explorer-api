41402b8056079466132e7def68f1c7777fca21ab
Maxim Talanov <mail@maxtalanov.ru>
2021-10-21 05:49:09 +0300

    inital commit v.2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
